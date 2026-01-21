f7c23b5cde67376503b64d96bc3b0716df08e85b
Christopher-preston03 <prestonchris03@gmail.com>
2026-01-21 02:50:13 -0600

    Cherry-pick merged Chris's branch

--- demo/gold_mock.py
+++ demo/gold_mock.py
@@ -1,16 +1,19 @@
+# gold_mock.py
+# Simulates Snowflake Gold-layer outputs for demos
+
 GOLD_ORDER_LOOKUP = {
     "order_456": {
         "item_category": "electronics",
-        "order_date": "2023-10-01",
-        "item_price_usd": 199.99,
+        "order_date": "2024-12-01",
+        "item_price_usd": 249.99,
     }
 }
 
 GOLD_RISK_LOOKUP = {
     "user_123": {
-        "refund_count_window": 2,
+        "refund_count_window": 1,
         "returnless_refund_count_window": 0,
         "chargeback_flag": False,
-        "address_distance_miles": 42.0,
+        "address_distance_miles": 12.5,
     }
 }
